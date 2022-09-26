ec0ceb206c512f8cc0595acc86360dc9be57e191
Roshan <roshanhegde@outlook.com>
2022-09-26 14:23:11 -0400

    ass

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
-print("hello world")
+from itertools import accumulate
+
+def check(*a):
+  valid = lambda x : x > 0 and x <= 15
+  accumulate((lambda x y x and y), map(valid, a))
+
+def main():
+  values = list("length", "width", )
+  
